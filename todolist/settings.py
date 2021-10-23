20c22cb1ac446181a2eb1f2ece5ab16994b46468
fznrsd@DESKTOP-DRD6N6C <fznrsd@DESKTOP-DRD6N6C.(MINGW64)git config --global user.name Faizan>
2021-10-23 18:27:29 +0530

    first time

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fpos5oyq4f7d9*0o&9tc+-(op*&4-kylup6&43p@3gx)(qnyu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["fznrsdtodo.herokuapp.com","127.0.0.1"]
 
 
 # Application definition
@@ -131,3 +133,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+django_heroku.settings(locals())
