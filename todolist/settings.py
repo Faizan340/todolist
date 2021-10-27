eef7a86a464957982c19f7c880852a10d9fd8bee
fznrsd@DESKTOP-DRD6N6C <fznrsd@DESKTOP-DRD6N6C.(MINGW64)git config --global user.name Faizan>
2021-10-27 12:13:32 +0530

    gg

--- todolist/settings.py
+++ todolist/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fpos5oyq4f7d9*0o&9tc+-(op*&4-kylup6&43p@3gx)(qnyu_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
